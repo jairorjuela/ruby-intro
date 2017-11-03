8f9be85c543c6aaf4a66eae11cbfe447e71f722e
david <davidndy@gmail.com>
2017-11-02 20:30:49 -0500

    Se resolvieron los retos de active-records 4 y 5

--- challenges/active-record/5-validations/source/app/models/rating.rb
+++ challenges/active-record/5-validations/source/app/models/rating.rb
@@ -1,2 +1,6 @@
 class Rating < ActiveRecord::Base
+	validates :coolness, :cuteness, :judge_id, :dog_id, { :presence => true }
+	validates :coolness, :cuteness,  numericality:{  greater_than_or_equal_to: 1 }
+	validates :coolness, :cuteness, numericality:{  :less_than_or_equal_to => 10 }
+	validates :judge_id,   uniqueness: { scope: :dog_id }
 end
